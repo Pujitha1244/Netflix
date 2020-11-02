d87f4e1f0b8932fbd5497645ce922f9524dcefff
Pujitha1244 <71977390+Pujitha1244@users.noreply.github.com>
2020-11-02 09:42:25 +0530

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,30 @@
+import React, { useState,useEffect } from 'react';
+import "./Nav.css";
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY > 100){
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return ()=> {
+            window.removeEventListener("scroll");
+        };
+    },[]);
+
+    return (
+        //  <div className={`nav ${show && "nav__black"}`}>
+        <div className={`nav ${show &&"nav__black"}`}>
+            <img
+                className="nav__logo"
+                src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
+                alt="Netflix Logo"
+                />
+        </div>
+    )
+}
+
+export default Nav
---
+++ build/precache-manifest.f0c2acaa051b5ee9b8730f61e236d03f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bda4032829de5e570e84ebb03992332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97cdc981d7d757ea817b",
+    "url": "/static/css/main.dd5f6fc5.chunk.css"
+  },
+  {
+    "revision": "3b7249c837af1d70486b",
+    "url": "/static/js/2.c962eaa2.chunk.js"
+  },
+  {
+    "revision": "25b54daaf8619b0ec755fedd71490812",
+    "url": "/static/js/2.c962eaa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97cdc981d7d757ea817b",
+    "url": "/static/js/main.97d36ed9.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
